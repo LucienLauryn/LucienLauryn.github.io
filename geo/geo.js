25c0d118b47193dd20b45dcd955feb92b6a6260f
U-Eureka\Anemone <Anemone@Eureka.(none)>
2014-10-26 03:41:07 -0400

    again

--- geo/geo.js
+++ geo/geo.js
@@ -16,7 +16,8 @@ var path = d3.geo.path().projection(projection);
 
 var color = d3.scale.quantize()
                     .range(["rgb(237,248,233)", "rgb(186,228,179)",
-                     "rgb(116,196,118)", "rgb(49,163,84)","rgb(0,109,44)"]);
+                     "rgb(116,196,118)", "rgb(49,163,84)","rgb(0,109,44)","rgb(37,248,233)", "rgb(86,228,179)",
+                     "rgb(16,196,11)", "rgb(49,13,84)","rgb(100,109,44)"]);
 
 d3.csv("statepopulation.csv", function(data) {
 	color.domain([
@@ -65,6 +66,7 @@ svg.selectAll("path")
                                 //If value exists…
                                 return color(value);
                         } 
+
            })
 	  })
 });
